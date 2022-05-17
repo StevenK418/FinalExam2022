313c97caefb83cf101fab37f6508c9b0e5bd75d4
Steven Kelly <S00200293@mail.itsligo.ie>
2022-05-17 11:29:21 +0100

    => Added final add property functionality.

--- FinalExam2022/AddProperty.xaml.cs
+++ FinalExam2022/AddProperty.xaml.cs
@@ -23,5 +23,51 @@ namespace FinalExam2022
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Constructs a new property object using the values passed in the UI
+        /// And passes this to the db manager to be added. 
+        /// </summary>
+        public void AddNewProperty()
+        {
+                //Instantiate new Rental Property object using values set in UI
+                RentalProperty rp = new RentalProperty
+                    (
+                       Decimal.Parse(TBX_Price.Text),
+                       RentalProperty.RentalType.House,
+                       //(!!!Enum parse issue arose in development, ran out of time)
+                       //Enum.Parse(RentalType, COMBX_Type.SelectedItem),
+                        TBX_Description.Text,
+                        TBX_Location.Text
+                     )
+                { };
+
+                //Create a new list (for brevity)
+                List<RentalProperty> properties = new List<RentalProperty>();
+            
+                //Add new ite to list
+                properties.Add(rp);
+
+                //Pass the new list(or item) to the database and add
+                DatabaseManager.AddToDatabase(properties);
+            }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Create new string list ofor the snum types
+            List<String> types = new List<string>();
+            types.Add("House");
+            types.Add("Flat");
+            types.Add("Share");
+
+            //Assign the list as the data source
+            COMBX_Type.ItemsSource = types;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Add the new Property to the DB. 
+            AddNewProperty();
+        }
     }
 }
