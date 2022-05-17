249b107e86478dd08e725d7c9e881203bd164c51
Steven Kelly <S00200293@mail.itsligo.ie>
2022-05-17 10:06:17 +0100

    => Added multiple test cases to the testing class.

--- FinalExam2022.tests/RentalPropertyTests.cs
+++ FinalExam2022.tests/RentalPropertyTests.cs
@@ -7,20 +7,42 @@ namespace FinalExam2022.tests
     [TestClass]
     public class RentalPropertyTests
     {
+
+        /// <summary>
+        /// Tests the increase rent amount method of the Rental Property Class
+        /// </summary>
         [TestMethod]
         public void TestIncreaseRentByTenPercent()
         {
             //Arrange
             decimal expectedResult = 150.00m;
 
+            //Act
             RentalProperty testProperty = new RentalProperty() { };
             testProperty.Price = 1500.00m;
-
-            //Act
             decimal actualResult = testProperty.IncreaseRentByPercentageAmount(.10m);
 
             //Assert
             Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [TestMethod]
+        [DataRow(.10, 150.0)]
+        [DataRow(.50, 750.0)]
+        [DataRow(.33, 495)]
+        public void TestIncreaseRentByMultiplePercentValues(double percent, double result)
+        {
+            //Arrange
+
+            //Act
+            RentalProperty testProperty = new RentalProperty() { };
+            testProperty.Price = 1500.00m;
+
+            //Get the result and cast to a double to meet datarow requirements
+            double actualResult = (double)testProperty.IncreaseRentByPercentageAmount((decimal)percent);
+
+            //Assert
+            Assert.AreEqual(result, actualResult);
         }
     }
 }
