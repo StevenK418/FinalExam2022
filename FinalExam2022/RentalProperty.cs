8de15f2542514f680256ac52e0563611d31b80b9
Steven Kelly <S00200293@mail.itsligo.ie>
2022-05-17 09:42:11 +0100

    => Added base RentalProperty Class.

---
+++ FinalExam2022/RentalProperty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam2022
+{
+    class RentalProperty
+    {
+        public int RentalPropertyID { get; set; }
+
+        public enum RentalType 
+        {
+            House,
+            Flat,
+            Share
+        }
+
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
